f38cc1e4ce41684b497da2414c4399ee17d43a98
Алина А. Отыргашева <alinaviot@KHPK.RU>
2022-03-30 15:01:21 +0700

    new project

---
+++ ClassLibraryPasswordTests/PasswordCheckerTests.cs
@@ -0,0 +1,82 @@
+﻿using ClassLibraryPassword;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClassLibraryPasswordTests
+{
+    [TestClass]
+    public class PasswordCheckerTests
+    {
+        [TestMethod]
+        public void ValidatePasswordTest()
+        {
+            //Arrange
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            //Act
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            //Arrange
+            string password = "Aq1$";
+
+            //Act
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            //Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Check_PasswordWithoutLowerSymbol()
+        {
+            //Arrange
+            string password = "ABVDF123$";
+            bool expected = false;
+
+            //Act
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Check_PasswordWithoutUpperSymbol()
+        {
+            //Arrange
+            string password = "asdfg123$";
+            bool expected = false;
+
+            //Act
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void Check_PasswordWithoutSpecialSymbol()
+        {
+            //Arrange
+
+            string password = "Asdfg1234";
+            bool expected = false;
+
+            //Act
+            bool actual = PasswordChecker.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ClassLibraryPasswordTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClassLibraryPasswordTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibraryPasswordTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2076a4ca-8784-4e91-82e1-bf3b6acc39cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
